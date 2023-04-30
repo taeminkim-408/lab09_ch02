9d382737590c2d11a6ec55b16842ca6f8bd2c541
kimbbanggyu <rladudrb042@gamil.com>
2023-04-30 18:50:31 +0900

    update printstar

--- printstar.c
+++ printstar.c
@@ -5,6 +5,10 @@ void main(){
 	printf("몇개의 별을 출력할까요?");
 	scanf("%d",&starcount);
 
-	for(int i=0; i<starcount; i++) printf("*");
+	for(int i=0; i<starcount; i++){
+		for(int j=0; j<starcount; j++)
+			printf("*");
+	 	printf("\n");
+	}
 	return;
 }
